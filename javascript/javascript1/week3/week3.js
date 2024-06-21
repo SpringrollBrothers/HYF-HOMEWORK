e81baff913f59a9df826514123843683de3ac7fe
Lu Lu <lulu@Lu.local>
2024-06-21 16:01:58 +0200

    when will we get there

--- javascript/javascript1/week3/week3.js
+++ javascript/javascript1/week3/week3.js
@@ -17,3 +17,22 @@ for (i = 0; i < names.length; i++) {
   }
 }
 console.log(names);
+
+//When will we be there
+
+function getHours(speed, destinationDistance) {
+  const hours = destinationDistance / speed;
+  function formatTime(hours) {
+    const wholeHours = Math.floor(hours);
+    const minutes = Math.round((hours - wholeHours) * 60);
+    console.log(wholeHours + " hours and " + minutes + " minutes");
+  }
+  formatTime(hours);
+}
+
+const travelInformation = {
+  speed: 100,
+  destinationDistance: 974,
+};
+
+getHours(travelInformation.speed, travelInformation.destinationDistance);
